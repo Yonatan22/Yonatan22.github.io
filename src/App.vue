da0d04de18968f6bd0525f6dd424f98101e56174
unknown <lang.yonatan@gmail.com>
2021-06-29 11:02:14 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,12 @@
           <div class="collapse navbar-collapse" id="navbarColor02">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/#/assignments"
+                <a class="nav-link active" aria-current="page" href=".#/assignments"
                   >Assignment</a
                 >
               </li>
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/#/archive"
+                <a class="nav-link active" aria-current="page" href=".#/archive"
                   >Archive</a
                 >
               </li>
